be3a1542b1be6c1cf118f1733b08f90dd93181ac
OttSahre <kobin4215@naver.com>
2025-07-17 21:47:13 +0900

    api-server 정상 실행

--- apps/api-server/src/main/java/com/skytracker/SkyTrackerApplication.java
+++ apps/api-server/src/main/java/com/skytracker/SkyTrackerApplication.java
@@ -2,11 +2,14 @@ package com.skytracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EntityScan(basePackages = "com.skytracker.entity")
+@EnableJpaRepositories(basePackages = "com.skytracker.repository")
 @EnableJpaAuditing
 @SpringBootApplication
 public class SkyTrackerApplication {
