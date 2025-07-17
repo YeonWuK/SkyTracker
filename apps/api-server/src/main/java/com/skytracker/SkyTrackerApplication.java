a02bf83af1cc2c392b8467b89cc14aa58397c715
OttSahre <kobin4215@naver.com>
2025-07-17 19:16:51 +0900

    init: initial project setup

---
+++ apps/api-server/src/main/java/com/skytracker/SkyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyTrackerApplication.class, args);
+	}
+
+}
