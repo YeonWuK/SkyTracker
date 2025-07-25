a8d596baccc1edfcc9f35644c5306684d2729ed6
kanghanseo <kanghanseo2015@gmail.com>
2025-07-25 16:28:48 +0900

    "ElasticSearch 집계 서비스 작성"

---
+++ apps/api-server/src/main/java/com/skytracker/dto/RouteAggregationDto.java
@@ -0,0 +1,17 @@
+package com.skytracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class RouteAggregationDto {
+    private String routeCode;
+    private String departureDate;
+    private String returnDate;
+    private long count;
+}
--- apps/elasticsearch/src/main/java/skytracker/repository/SearchRepository.java
+++ apps/elasticsearch/src/main/java/skytracker/repository/SearchRepository.java
@@ -1,8 +1,10 @@
 package skytracker.repository;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 import skytracker.document.SearchLogsDocument;
 
+@Repository
 public interface SearchRepository extends ElasticsearchRepository<SearchLogsDocument, Long> {
 
 }
