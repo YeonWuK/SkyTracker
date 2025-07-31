4ebe9c7f123edb092cf280b3a9e3074a1d8e480c
YeonWoo <kobin4215@naver.com>
2025-07-31 16:42:07 +0900

    [feat] 항공권 검색용 Dto 필드명 및 구조 정리

--- apps/common/src/main/java/com/skytracker/common/dto/flightSearch/RoundTripFlightSearchResponseDto.java
+++ apps/common/src/main/java/com/skytracker/common/dto/flightSearch/RoundTripFlightSearchResponseDto.java
@@ -1,2 +1,36 @@
-package com.skytracker.common.dto.flightSearch;public class RoundTripFlightSearchResponseDto {
+package com.skytracker.common.dto.flightSearch;
+
+import com.skytracker.common.dto.enums.TravelClass;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoundTripFlightSearchResponseDto {
+    private String airlineCode;
+    private String airlineName;
+    private String flightNumber;
+
+    private String departureAirport;
+    private String arrivalAirport;
+    private String outboundDepartureTime;
+    private String outboundArrivalTime;
+    private String outboundDuration;
+
+    private String returnDepartureTime;
+    private String returnArrivalTime;
+    private String returnDuration;
+
+    private TravelClass travelClass;
+    private int numberOfBookableSeats;
+    private boolean hasCheckedBags;
+
+    private String currency;
+    private int price;
+    private boolean isRefundable;
+    private boolean isChangeable;
 }
